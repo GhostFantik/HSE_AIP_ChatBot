409ddda14bbbd49ee71d326a7186494d9f730ed4
GhostFantik <ghostfantik@gmail.com>
2021-02-26 22:59:45 +0500

    VkAdapter was made! (3)

--- TgAdapter.py
+++ TgAdapter.py
@@ -1,16 +1,14 @@
-# token 1536002137:AAEW6IhEfsvKx_F42KeFQt_tlkXWIVlYoVo
-
 import os
 import dotenv
 import telebot
 import requests
-import json
 
 dotenv.load_dotenv()
 
 url = os.getenv('server_message_url')
 
-bot = telebot.TeleBot("1536002137:AAEW6IhEfsvKx_F42KeFQt_tlkXWIVlYoVo")
+bot = telebot.TeleBot(os.getenv('tg_token'))
+
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
