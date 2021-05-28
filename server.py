2af597cb501c244f82678c7356ebc748849ecb99
GhostFantik <ghostfantik@gmail.com>
2021-05-28 22:35:23 +0300

    Logger was added

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from loguru import logger
 from BotLogic import message_handle
 import db
 
@@ -6,11 +7,14 @@ import db
 app = Flask(__name__)
 db.connect()
 
+logger.add('logs.txt')
+
 
 @app.route('/message', methods=['POST'])
 def hello():
     user = request.form.get('user')
     source = request.form.get('source')
     message = request.form.get('message')
+    logger.info(f'New message from {source}-client {user} - {message}')
     return message_handle(user, source, message)
 
